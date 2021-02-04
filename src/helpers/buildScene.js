1e73498c7e48cedaf0dd21e6efb74522b76e8b31
savaughn <svaughn57@gmail.com>
2021-02-03 21:19:47 -0600

    Revert "updated map"
    This reverts commit 3bb9fb92d6df6b5542ed7b702342a07a01dd2980.

--- src/helpers/buildScene.js
+++ src/helpers/buildScene.js
@@ -6,9 +6,9 @@ export default function buildScene(scene) {
     scene.map = scene.make.tilemap({
         key: 'map'
     });
-    scene.tileset = scene.map.addTilesetImage('Terrain', 'tiles');
+    scene.tileset = scene.map.addTilesetImage('platformer', 'tiles');
 
-    scene.background = scene.map.createLayer('sky', scene.tileset, 0, 400);
-    scene.platforms = scene.map.createLayer('ground', scene.tileset, 0, 400);
+    scene.background = scene.map.createLayer('Tile Layer 2', scene.tileset, 750, 400);
+    scene.platforms = scene.map.createLayer('platform', scene.tileset, 750, 475);
     scene.platforms.setCollisionByExclusion(-1, true);
 }
