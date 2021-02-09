ec089ec607c1237ccd5b27108a6d12385b1ed013
savaughn <svaughn57@gmail.com>
2021-02-09 15:09:07 -0600

    added 2p

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -12,7 +12,7 @@ class GameScene extends Phaser.Scene {
         buildScene(this);
         this.hero = new Hero({
             scene: this,
-            key: 'hero',
+            key: 'frog',
             x: 1100,
             y: 800,
             enablePhysics: true
@@ -20,13 +20,31 @@ class GameScene extends Phaser.Scene {
         this.input.gamepad?.getPad(0)
         );
 
+        this.frog = null;
+
+        if (this.input.gamepad?.pad2) {
+            this.frog = new Hero({
+                scene: this,
+                key: 'frog',
+                x: 1000,
+                y: 800,
+                enablePhysics: true
+            },
+            this.input.gamepad?.getPad(1)
+            );
+            this.physics.add.collider(this.frog, this.platforms);
+            console.log(this.frog.setTint(0xef7564));
+        }
+
         this.physics.add.collider(this.hero, this.platforms);
         this.cameras.main.setZoom(4);
         this.cameras.main.startFollow(this.hero, true, 0.8, 0.0, 0, 50);
+
     }
 
     update(time, delta) {
         this.hero.update(time, delta);
+        this.frog?.update(time, delta);
     }
 }
 
