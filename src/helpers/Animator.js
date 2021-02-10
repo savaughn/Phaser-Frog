fe87fedaadc0c3009c599f8d41566144f6cc1522
savaughn <svaughn57@gmail.com>
2021-02-10 10:18:04 -0600

    added animation states / error handling

--- src/helpers/Animator.js
+++ src/helpers/Animator.js
@@ -2,11 +2,31 @@ export default class Animator {
     constructor(config) {
         this.sprite = config.sprite;
         this.type = config.type;
+        this.animations = {
+            idle: 'idle',
+            walk: 'walk',
+            run: 'run',
+            jumpUp: 'jump-up',
+            jumpDown: 'jump-down',
+            crouch: 'crouch',
+            roll: 'roll',
+            hardLand: 'hard-land',
+            softLand: 'soft-land',
+            slide: 'slide'
+        };
+    }
+
+    errorHandle(anim) {
+        let anims = Object.values(this.animations);
+        let results = anims.filter(item => {
+            return item.match(`(${ anim })`);
+        });
+        console.error(`Invalid animation name: '${anim}'\nDid you mean:${ results.map(item => ` ${ item}`)}?`);
     }
 
     getAnimation(animation) {
         switch (animation) {
-            case 'idle': {
+            case this.animations.idle: {
                 if(this.type === 'frog') {
                     return 'frog-idle_out';
                 } else {
@@ -46,6 +66,36 @@ export default class Animator {
                     return 'frog-crouch_out';
                 } else {
                     return 'crouch';
+                }
+            }
+            case 'roll': {
+                if (this.type === 'frog') {
+                    return 'frog-jump_up_out';
+                } else {
+                    return 'roll';
+                }
+            }
+            case 'hard-land': {
+                if (this.type === 'frog') {
+                    return 'frog-crouch_out';
+                } else {
+                    return 'hard-land';
+                }
+            }
+            case 'slide': {
+                if (this.type === 'frog') {
+                    return 'frog-crouch_out';
+                } else {
+                    return 'slide';
+                }
+            }
+            default: {
+                this.errorHandle(animation);
+                // console.error(`missing animation: ${ animation }`);
+                if(this.type === 'frog') {
+                    return 'frog-idle_out';
+                } else {
+                    return 'idle'
                 }
             }
         }
